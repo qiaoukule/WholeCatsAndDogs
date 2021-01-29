502023857f47ead9d1d045e0b316196ef6adebf4
荍荍 <675061026@qq.com>
2021-01-29 11:00:21 +0800

    终于搞定路由了

---
+++ admin/src/Pages/AdminIndex.js
@@ -0,0 +1,81 @@
+//直接拿antd的布局例子改写成hooks
+//路由部分：刷新后，页面会空白，渲染不出来
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import '../static/css/AdminIndex.css'
+import React, { useState } from 'react';
+import { Route, Link, Switch, BrowserRouter as Router, } from 'react-router-dom';
+import AddArticle from '../Pages/AddArticle'
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+function AdminIndex(props) {
+
+    const { children, location, loading } = props;
+
+    const [collapsed, setCollapsed] = useState(false)
+
+    const onCollapse = collapsed => {
+        console.log(collapsed);
+        setCollapsed(collapsed)
+    };
+
+    return (
+    <Router>
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+                <div className="logo" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                    <Menu.Item key="1" icon={<PieChartOutlined />}>
+                        文章管理
+                    </Menu.Item>
+                    <SubMenu key="sub1" icon={<DesktopOutlined />} title="文章管理">
+                        <Menu.Item key="3"> 
+                            <Link to="/AdminIndex/addArticle">添加文章</Link>
+                         </Menu.Item>
+                        <Menu.Item key="4">文章列表</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                        <Menu.Item key="6">Team 1</Menu.Item>
+                        <Menu.Item key="8">Team 2</Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key="9" icon={<FileOutlined />}>
+                        留言管理
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout className="site-layout">
+                <Content style={{ margin: '0 16px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>后台管理系统</Breadcrumb.Item>
+                        <Breadcrumb.Item>工作台</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                        <div>
+                            <Switch>
+                               {/*  错误 children={AddArticle} */}
+                                <Route path="/AdminIndex/addArticle"  >
+                                    <AddArticle />
+                                </Route>
+                                <Route path="/AdminIndex/one" children={<h3>One</h3>} />
+                                <Route path="/two" children={<h3>Two</h3>} />
+                            </Switch>
+                            {children}
+                        </div>
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+        </Layout>
+    </Router>
+    );
+}
+
+export default AdminIndex
--- admin/src/Pages/Main.js
+++ admin/src/Pages/Main.js
@@ -3,11 +3,13 @@
 import React from 'react';
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 import Login from './Login'
+import AdminIndex from './AdminIndex'
 
 function Main() {
     return(
         <Router>
-            <Route path='/login/' exact component={Login} />
+            <Route path='/login/'  component={Login} />
+            <Route path='/AdminIndex/'  component={AdminIndex} />
         </Router>
     )
 }
